dcf742bb8fc1d3e9051efc066eb143b0f4842d51
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2022-02-16 18:07:31 +0530

    updated

---
+++ javascript.js
@@ -0,0 +1,24 @@
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+console.log(navMenuAnchorTags);
+
+
+for (var i = 0; i < navMenuAnchorTags.length; i++) {
+    navMenuAnchorTags[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        console.log(this.textContent);
+        var targetSection = document.getElementById(targetSectionID);
+        console.log(targetSection);
+        //    interval = setInterval(scrollVertically, 20, targetSection);
+
+        var interval = setInterval(function () {
+            // scrollVertically(targetSection);
+            var targetSectionCoordinates = targetSection.getBoundingClientRect();
+            if(targetSectionCoordinates.top <= 0){
+                clearInterval(interval);
+                return;
+            }
+            window.scrollBy(0, 50);
+        }, 20);
+    });
+}
